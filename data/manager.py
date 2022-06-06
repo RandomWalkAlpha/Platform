dba347f6ec4c5e6e5a54054822ba51cf6163fcc2
402010848@qq.com <402010848@qq.com>
2022-06-06 18:48:23 +0800

    update manager and recover db

--- data/manager.py
+++ data/manager.py
@@ -1,33 +1,36 @@
 import abc
-import redis
 from pandas import DataFrame
+from store.database import RedisController
 
 
 class SignalDataManager(abc.ABC):
+    """
+    This manager is to manage signal data and interact with task queue.
+    """
+
     def __init__(self):
-        pass
+        self.db = RedisController()
 
     def get(self, signal_name: str, begin: int, end: int) -> DataFrame:
         """
-        首先去 redis 中查找表达式，其次全量计算，返回因子值（初步实现）
+        在 redis 中查找表达式，返回因子值（初步实现）
         """
         raise NotImplementedError
 
     def put(self, signal_name: str, expression: str):
         """
-        存入表达式至 redis
+        存入表达式至 redis / file
         """
         raise NotImplementedError
 
 
 class SignalStorageManager(abc.ABC):
     """
-    This class is responsible for redis managing
+    This manager is responsible for redis and meta-info managing.
     """
 
-    def __init__(self, host: str = 'localhost', port: int = 6379, password: str = None):
-        self.pool = redis.ConnectionPool(host, port)
-        self.connection = redis.Redis(password=password, connection_pool=self.pool, decode_responses=True)
+    def __init__(self):
+        self.db = RedisController()
 
     def get(self, signal_name: str):
         raise NotImplementedError
